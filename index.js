36a9db9da4583515c6aa19576898daef136af867
Grace Durant <gdurant34@gmail.com>
2022-09-22 10:50:12 -0700

    commit tree 39c02b583077bce50fbf3d49643c3496406d071b

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
-// Add your code here
+// function submitData(name, email) {
+//     return fetch("http://localhost:3000/users", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json",
+//         },
+//         body: JSON.stringify({
+//             name: name,
+//             email: email,
+//         }),
+//     })  
+//     .then((resp) => resp.json())
+//     .then((user) => handleNewId(user))
+//     .catch((error) => {errorMessage(error)})
+// };
+
+// function handleNewId(user) {
+//     const body = document.querySelector('body');
+//     const newId = user.id;
+//     const p = document.createElement('p');
+//     p.textContent = newId;
+//     body.appendChild(p);
+//     // console.log(body);
+// };
+
+// function errorMessage(error) {
+//     const body = document.querySelector('body');
+//     const message = error.message
+//     const p = document.createElement('p');
+//     p.textContent = message;
+//     body.appendChild(p);
+//     console.log(error);
+// }
+    
+// submitData("Steve", "steve@steve.com");
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then((resp) => resp.json())
+    .then((user) => handleNewId(user))
+    .catch((error) => errorMessage(error))
+}
+
+function handleNewId(user) {
+    const body = document.querySelector('body');
+    const p = document.createElement('p');
+    const newId = user.id;
+    p.textContent = newId;
+    body.appendChild(p);
+};
+
+function errorMessage(error) {
+    const body = document.querySelector('body');
+    const p = document.createElement('p');
+    const msg = error.message;
+    p.textContent = msg;
+    body.appendChild(p);
+}
